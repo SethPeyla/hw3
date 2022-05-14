f000ac23bb98b59771b7cc34100416c1b8536ca6
SethPeyla <seth.peyla@kellogg.northwestern.edu>
2022-05-14 18:08:37 +0000

    saved work 5 on 05/14/22

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,21 @@ end
 
 def new
     @post = Post.new
+    @post["place_id"] = params["place_id"]
+end
+
+def show
+    @post = Post.find_by({ "id" => params["id"] })
+    @place = Place.find_by({ "id" => @post["place_id"] })
+end
+
+def create
+    @post = Post.new
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post.save
+    redirect_to "/posts"
 end
 
 end
