c0b6ed5acaf0d42c564da29626b03711418363c4
SethPeyla <seth.peyla@kellogg.northwestern.edu>
2022-05-14 18:12:18 +0000

    saved work 6 on 05/14/22

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,8 +19,9 @@ def create
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
+    @post["place_id"] = params["post"]["place_id"]
     @post.save
-    redirect_to "/posts"
+    redirect_to "/places"
 end
 
 end
