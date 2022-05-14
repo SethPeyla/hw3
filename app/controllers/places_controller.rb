81c56556548652fbadbec18b671668679ab80c5d
SethPeyla <seth.peyla@kellogg.northwestern.edu>
2022-05-14 16:48:50 +0000

    saved work on 5/14/22

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,18 @@
 class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+    end
+
+    def show
+        @place = Place.find_by({ "id" => params["id"] })
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+    end
+
 end
